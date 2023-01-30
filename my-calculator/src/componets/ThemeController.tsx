4aa748d3d4e8be89e668cf2231d2a2a9228349f8
luis Colina <colina.1990lz@gmail.com>
2023-01-30 10:40:49 -0400

    alternative themes and functionality added

--- my-calculator/src/componets/ThemeController.tsx
+++ my-calculator/src/componets/ThemeController.tsx
@@ -1,16 +1,25 @@
-import { FC, useContext } from 'react'
+import { FC, useContext, Dispatch, SetStateAction } from 'react'
 import style from "./themeController.module.css"
-import { ThemeContext } from "../App";
+import { ThemeContext, themeContextType } from "../App";
+import React from 'react';
 
-const ThemeController:FC = () => {
+interface propis {
+  changeTheme: (arg0:themeContextType) => void
+}
+
+const ThemeController:FC<propis> = ({changeTheme}) => {
   const theme = useContext(ThemeContext)
+
+  function changeRen(e: React.ChangeEvent<HTMLInputElement>) {
+    changeTheme(+e.target.value as themeContextType)
+  }
 
   return (
     <header className={`${style.header} ${theme === 0? "light-switch" : theme === 1 ? "dark-switch" : "purple-switch"}`}>
       <h1>calc</h1>
       <div>
         <label htmlFor="theme-selector">THEME</label>
-        <input type="range" name="theme-selector" id="theme_selector" min={1} max={3} className={`${style.range} ${theme === 0? "light-input" : theme === 1 ? "dark-input" : "purple-input"}`}/>
+        <input type="range" onChange={changeRen}  name="theme-selector" id="theme_selector" min={0} max={2}  className={`${style.range} ${theme === 0? "light-input" : theme === 1 ? "dark-input" : "purple-input"}`}/>
         <ul className={style.list} id="theme-selec">
           <li>1</li>
           <li>2</li>
